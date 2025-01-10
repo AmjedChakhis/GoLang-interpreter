b723ad05f75dccd55ade4fa845a7d88919591664
AmjedChakhis <cd.amjed.riham@gmail.com>
2025-01-10 17:35:59 +0100

    Implement lexer

---
+++ core/lexer/lexer.go
@@ -0,0 +1,195 @@
+package lexer
+
+import (
+	"unicode/utf8"
+
+	"github.com/AmjedChakhis/GoLang-interpreter/core/token"
+	"github.com/AmjedChakhis/GoLang-interpreter/utilities"
+)
+
+type Lexer struct {
+	input      string // the string to tokenize
+	currentPos int    // points to the current position in the input
+	readPos    int    // current read position after the current char
+	char       rune   // the current char (byte as the binary representation of )
+}
+
+func InitLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar() // READ FIRST CHAR
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	l.ignoreWhiteSpace()
+
+	switch l.char {
+	case '=':
+		if l.peek() == '=' {
+			prev := l.char
+			l.readChar()
+			tok = token.CreateToken(token.EQUAL, string(prev)+string(l.char))
+		} else {
+			tok = token.CreateToken(token.ASSIGN, string(l.char))
+		}
+	case '&':
+		if l.peek() == '&' {
+			prev := l.char
+			l.readChar()
+			tok = token.CreateToken(token.AND, string(prev)+string(l.char))
+		} else {
+			tok = token.CreateToken(token.ILLEGAL, string(l.char))
+		}
+
+	case '|':
+		if l.peek() == '|' {
+			prev := l.char
+			l.readChar()
+			tok = token.CreateToken(token.OR, string(prev)+string(l.char))
+		} else {
+			tok = token.CreateToken(token.ILLEGAL, string(l.char))
+		}
+	case '+':
+		if l.peek() == '+' {
+			prev := l.char
+			l.readChar()
+			tok = token.CreateToken(token.INCREMENT, string(prev)+string(l.char))
+		} else {
+			tok = token.CreateToken(token.PLUS, string(l.char))
+		}
+	case '-':
+		if l.peek() == '-' {
+			prev := l.char
+			l.readChar()
+			tok = token.CreateToken(token.DECREMENT, string(l.char)+string(prev))
+		} else {
+			tok = token.CreateToken(token.MINUS, string(l.char))
+		}
+	case '/':
+		tok = token.CreateToken(token.SLASH, string(l.char))
+	case '%':
+		tok = token.CreateToken(token.MODULO, string(l.char))
+	case '*':
+		tok = token.CreateToken(token.STAR, string(l.char))
+	case '!':
+		if l.peek() == '=' {
+			prev := l.char
+			l.readChar()
+			tok = token.CreateToken(token.NOT_EQUAL, string(prev)+string(l.char))
+		} else {
+			tok = token.CreateToken(token.EX_MARK, string(l.char))
+		}
+	case '<':
+		if l.peek() == '=' {
+			prev := l.char
+			l.readChar()
+			tok = token.CreateToken(token.LT_OR_EQ, string(prev)+string(l.char))
+		} else {
+			tok = token.CreateToken(token.LT, string(l.char))
+		}
+	case '>':
+		if l.peek() == '=' {
+			prev := l.char
+			l.readChar()
+			tok = token.CreateToken(token.GT_OR_EQ, string(prev)+string(l.char))
+		} else {
+			tok = token.CreateToken(token.GT, string(l.char))
+		}
+	case ')':
+		tok = token.CreateToken(token.RP, string(l.char))
+	case '(':
+		tok = token.CreateToken(token.LP, string(l.char))
+	case '{':
+		tok = token.CreateToken(token.LCB, string(l.char))
+	case '}':
+		tok = token.CreateToken(token.RCB, string(l.char))
+	case '[':
+		tok = token.CreateToken(token.LB, string(l.char))
+	case ']':
+		tok = token.CreateToken(token.RB, string(l.char))
+	case ',':
+		tok = token.CreateToken(token.COMMA, string(l.char))
+	case ';':
+		tok = token.CreateToken(token.S_COLON, string(l.char))
+	case '"':
+		tok = token.CreateToken(token.STRING, l.ReadString())
+	case 0:
+		tok = token.CreateToken(token.FILE_ENDED, string(rune(0)))
+	default:
+		if utils.IsLetter(l.char) {
+			ident := l.ReadIdentifier()
+			tok = token.CreateToken(token.GetIdentifierTokenType(ident), ident)
+			return tok
+		} else if utils.IsDigit(l.char) {
+			num := l.ReadNumber()
+			tok = token.CreateToken(token.INT, num)
+			return tok // this prevents calling read char which is already done with the method ReadNumber()
+		} else {
+			tok = token.CreateToken(token.ILLEGAL, string(l.char))
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+// HELPER FUNCTIONS
+func (l *Lexer) readChar() {
+
+	if l.readPos >= utf8.RuneCount([]byte(l.input)) {
+		l.char = 0 // SET THE CURRENT CHAR TO NUL CHARACTER (TO INDICATE THE TERMINATION OF THE STRING)
+	} else {
+		r, size := utf8.DecodeRuneInString(l.input[l.readPos:])
+		l.char = r
+		l.currentPos = l.readPos
+		l.readPos += size
+	}
+}
+
+// read string literals
+func (l *Lexer) ReadString() string {
+	currPosition := l.currentPos + 1
+	for {
+		l.readChar()
+		if l.char == '"' || l.char == 0 {
+			break
+		}
+	}
+	return l.input[currPosition:l.currentPos]
+}
+
+func (l *Lexer) peek() rune {
+	if l.readPos >= utf8.RuneCount([]byte(l.input)) {
+		return 0
+	}
+	return rune(l.input[l.readPos])
+}
+
+func (l *Lexer) ReadIdentifier() string {
+	currPosition := l.currentPos
+
+	//identifiers can't start with numbers
+	if currPosition == 0 && utils.IsDigit(l.char) {
+		return ""
+	}
+
+	for utils.IsDigit(l.char) || utils.IsLetter(l.char) {
+		l.readChar()
+	}
+	return l.input[currPosition:l.currentPos]
+}
+
+func (l *Lexer) ReadNumber() string {
+	currentPos := l.currentPos
+	for utils.IsDigit(l.char) {
+		l.readChar()
+	}
+	return l.input[currentPos:l.currentPos]
+}
+
+func (l *Lexer) ignoreWhiteSpace() {
+	for l.char == ' ' || l.char == '\t' || l.char == '\n' || l.char == '\r' {
+		l.readChar()
+	}
+}
---
+++ core/lexer/lexer_test.go
@@ -0,0 +1,324 @@
+package lexer
+
+import (
+	"log"
+	"testing"
+
+	"github.com/AmjedChakhis/GoLang-interpreter/core/token"
+)
+
+func TestNext(t *testing.T) {
+	MOCK1_TEST := Mock0
+
+	var tests = NextData0
+	myLexer := InitLexer(MOCK1_TEST)
+
+	for i, et := range tests {
+
+		calculatedToken := myLexer.NextToken()
+
+		// test the token type
+		if et.expectedTokenType != calculatedToken.Type {
+			log.Fatalf("tests index %d -> tokenType wrong, expected:[%q] and got:[%q]",
+				i, et.expectedTokenType, calculatedToken.Type)
+		}
+
+		// test the token literal value
+		if et.expectedValue != calculatedToken.Value {
+			log.Fatalf("tests index %d -> token value is wrong, expected:[%q] and got:[%q]",
+				i, et.expectedValue, calculatedToken.Value)
+		}
+	}
+}
+
+func TestNext2(t *testing.T) {
+	MOCK1_TEST := Mock1
+
+	var tests = NextData1
+	myLexer := InitLexer(MOCK1_TEST)
+
+	for i, et := range tests {
+		calculatedToken := myLexer.NextToken()
+		// test the token type
+		if et.expectedTokenType != calculatedToken.Type {
+			log.Fatalf("tests index %d -> token value is wrong, expected:[%q] and got:[%q] %s:%s",
+				i, et.expectedValue, calculatedToken.Value, et.expectedValue, calculatedToken.Value)
+		}
+
+		// test the token literal value
+		if et.expectedValue != calculatedToken.Value {
+			log.Fatalf("tests index %d -> token value is wrong, expected:[%q] and got:[%q]",
+				i, et.expectedValue, calculatedToken.Value)
+		}
+	}
+}
+
+func TestNext3(t *testing.T) {
+	MOCK1_TEST := Mock2
+
+	var tests = NextTestData
+
+	myLexer := InitLexer(MOCK1_TEST)
+
+	for i, et := range tests {
+		calculatedToken := myLexer.NextToken()
+		// test the token type
+
+		if et.expectedTokenType != calculatedToken.Type {
+			log.Fatalf("tests index %d -> tokenType wrong, expected:[%d] and got:[%d]",
+				i, et.expectedTokenType, calculatedToken.Type)
+		}
+
+		// test the token literal value
+		if et.expectedValue != calculatedToken.Value {
+			log.Fatalf("tests index %d -> token value is wrong, expected:[%q] and got:[%q] %s:%s",
+				i, et.expectedValue, calculatedToken.Value, et.expectedValue, calculatedToken.Value)
+		}
+	}
+}
+
+// Test data
+var (
+	NextTestData = []struct {
+		expectedTokenType token.TokenType
+		expectedValue     string
+	}{
+		{expectedTokenType: token.DEF, expectedValue: "def"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val1"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.INT, expectedValue: "30"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.DEF, expectedValue: "def"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val2"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.INT, expectedValue: "3"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.DEF, expectedValue: "def"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "add"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.FUNCTION, expectedValue: "function"},
+		{expectedTokenType: token.LP, expectedValue: "("},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "x"},
+		{expectedTokenType: token.COMMA, expectedValue: ","},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "y"},
+		{expectedTokenType: token.RP, expectedValue: ")"},
+		{expectedTokenType: token.LCB, expectedValue: "{"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "x"},
+		{expectedTokenType: token.PLUS, expectedValue: "+"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "y"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+		{expectedTokenType: token.RETURN, expectedValue: "return"},
+		{expectedTokenType: token.TRUE, expectedValue: "true"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+		{expectedTokenType: token.RCB, expectedValue: "}"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.DEF, expectedValue: "def"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "result"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "add"},
+		{expectedTokenType: token.LP, expectedValue: "("},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val1"},
+		{expectedTokenType: token.COMMA, expectedValue: ","},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val2"},
+		{expectedTokenType: token.RP, expectedValue: ")"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.EX_MARK, expectedValue: "!"},
+		{expectedTokenType: token.MINUS, expectedValue: "-"},
+		{expectedTokenType: token.STAR, expectedValue: "*"},
+		{expectedTokenType: token.INT, expectedValue: "7"},
+		{expectedTokenType: token.SLASH, expectedValue: "/"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.IF, expectedValue: "if"},
+		{expectedTokenType: token.LP, expectedValue: "("},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val1"},
+		{expectedTokenType: token.LT, expectedValue: "<"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val2"},
+		{expectedTokenType: token.RP, expectedValue: ")"},
+		{expectedTokenType: token.LCB, expectedValue: "{"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val1"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.INT, expectedValue: "7777"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+		{expectedTokenType: token.RCB, expectedValue: "}"},
+
+		{expectedTokenType: token.ELSE, expectedValue: "else"},
+		{expectedTokenType: token.LCB, expectedValue: "{"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val2"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.INT, expectedValue: "7777"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+		{expectedTokenType: token.RCB, expectedValue: "}"},
+
+		{expectedTokenType: token.INT, expectedValue: "10"},
+		{expectedTokenType: token.EQUAL, expectedValue: "=="},
+		{expectedTokenType: token.INT, expectedValue: "10"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.INT, expectedValue: "10"},
+		{expectedTokenType: token.NOT_EQUAL, expectedValue: "!="},
+		{expectedTokenType: token.INT, expectedValue: "7"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.INT, expectedValue: "10"},
+		{expectedTokenType: token.LT_OR_EQ, expectedValue: "<="},
+		{expectedTokenType: token.INT, expectedValue: "20"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.INT, expectedValue: "10"},
+		{expectedTokenType: token.GT_OR_EQ, expectedValue: ">="},
+		{expectedTokenType: token.INT, expectedValue: "0"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.INT, expectedValue: "10"},
+		{expectedTokenType: token.INCREMENT, expectedValue: "++"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+		{expectedTokenType: token.INT, expectedValue: "10"},
+		{expectedTokenType: token.DECREMENT, expectedValue: "--"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.LB, expectedValue: "["},
+		{expectedTokenType: token.INT, expectedValue: "2222"},
+		{expectedTokenType: token.COMMA, expectedValue: ","},
+		{expectedTokenType: token.INT, expectedValue: "7777"},
+		{expectedTokenType: token.RB, expectedValue: "]"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+	}
+
+	NextData0 = []struct {
+		expectedTokenType token.TokenType
+		expectedValue     string
+	}{
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.PLUS, expectedValue: "+"},
+		{expectedTokenType: token.LP, expectedValue: "("},
+		{expectedTokenType: token.RP, expectedValue: ")"},
+		{expectedTokenType: token.LCB, expectedValue: "{"},
+		{expectedTokenType: token.RCB, expectedValue: "}"},
+		{expectedTokenType: token.COMMA, expectedValue: ","},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+	}
+
+	NextData1 = []struct {
+		expectedTokenType token.TokenType
+		expectedValue     string
+	}{
+		{expectedTokenType: token.DEF, expectedValue: "def"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val1"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.INT, expectedValue: "30"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.DEF, expectedValue: "def"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val2"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.INT, expectedValue: "3"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.DEF, expectedValue: "def"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "add"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.FUNCTION, expectedValue: "function"},
+		{expectedTokenType: token.LP, expectedValue: "("},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "x"},
+		{expectedTokenType: token.COMMA, expectedValue: ","},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "y"},
+		{expectedTokenType: token.RP, expectedValue: ")"},
+		{expectedTokenType: token.LCB, expectedValue: "{"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "x"},
+		{expectedTokenType: token.PLUS, expectedValue: "+"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "y"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+		{expectedTokenType: token.RCB, expectedValue: "}"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+		{expectedTokenType: token.DEF, expectedValue: "def"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "result"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "add"},
+		{expectedTokenType: token.LP, expectedValue: "("},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val1"},
+		{expectedTokenType: token.COMMA, expectedValue: ","},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "val2"},
+		{expectedTokenType: token.RP, expectedValue: ")"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+
+		{expectedTokenType: token.CLASS, expectedValue: "class"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "helloworld"},
+		{expectedTokenType: token.LCB, expectedValue: "{"},
+		{expectedTokenType: token.DEF, expectedValue: "def"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "var1"},
+		{expectedTokenType: token.ASSIGN, expectedValue: "="},
+		{expectedTokenType: token.INT, expectedValue: "444"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+		{expectedTokenType: token.CONSTRUCTOR, expectedValue: "constructor"},
+		{expectedTokenType: token.LP, expectedValue: "("},
+		{expectedTokenType: token.RP, expectedValue: ")"},
+		{expectedTokenType: token.LCB, expectedValue: "{"},
+		{expectedTokenType: token.RCB, expectedValue: "}"},
+		{expectedTokenType: token.FUNCTION, expectedValue: "function"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "toString"},
+		{expectedTokenType: token.LP, expectedValue: "("},
+		{expectedTokenType: token.RP, expectedValue: ")"},
+		{expectedTokenType: token.LCB, expectedValue: "{"},
+		{expectedTokenType: token.IDENTIFIER, expectedValue: "out"},
+		{expectedTokenType: token.LP, expectedValue: "("},
+		{expectedTokenType: token.STRING, expectedValue: "hello world"},
+		{expectedTokenType: token.RP, expectedValue: ")"},
+		{expectedTokenType: token.S_COLON, expectedValue: ";"},
+		{expectedTokenType: token.RCB, expectedValue: "}"},
+
+		{expectedTokenType: token.RCB, expectedValue: "}"},
+	}
+
+	Mock2 = `def val1 = 30;
+	
+	def val2 = 3;
+	def add = function(x, y) {
+		x + y;
+		return true;
+	};
+	
+	def result = add(val1, val2);
+	
+	!-*7/;
+
+	if(val1 < val2) {
+		val1 = 7777;
+	} else {
+		val2=7777;
+	}
+	10 == 10;
+	10 != 7;
+	10 <= 20;
+	10 >= 0;
+	10++;
+	10--;
+	[2222,7777];
+	`
+
+	Mock1 = `def val1 = 30;
+	def val2 = 3;
+	def add = function(x, y) {
+		x + y;
+	};
+	def result = add(val1, val2);
+	class helloworld {
+		
+		def var1=444;
+
+		constructor() {
+
+		}
+
+		function toString() {
+			out("hello world");
+		}
+
+	}`
+
+	Mock0 = "=+(){},;"
+)
